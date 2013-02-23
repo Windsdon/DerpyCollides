97c7e58d3bdc94aab88b7e6c9360814320d282a0
Windsdon <jpfm2210@gmail.com>
2013-02-23 20:49:28 -0300

    Basic collision system working. Needs some changes.

--- DerpyCollides/src/windsdon/derp/derpycollides/DerpyGame.java
+++ DerpyCollides/src/windsdon/derp/derpycollides/DerpyGame.java
@@ -53,8 +53,12 @@ public class DerpyGame implements Runnable, MouseListener {
     }
 
     private void init() {
-        addBox(new Box(100, 100, 100, 100));
-        addBox(new Box(new Rectangle2D.Double(100, 600, 100, 100), Color.red, new Physics(500, -1000, 0, 1000, 1000, false)));
+        //addBox(new Box(100, 100, 100, 100));
+        addBox(new Box(new Rectangle2D.Double(100, 300, 100, 100), Color.red, new Physics(500, -1000, 0, 1000, 1000, false)));
+        addBox(new Box(new Rectangle2D.Double(1080, 300, 100, 100), Color.red, new Physics(-500, -1000, 100, 1000, 1000, false)));
+        addBox(new Box(new Rectangle2D.Double(0, 200, 1280, 20), Color.black, new Physics()));
+        addBox(new Box(new Rectangle2D.Double(0, 500, 1280, 20), Color.black, new Physics()));
+        addBox(new Box(new Rectangle2D.Double(630, 0, 20, 720), Color.black, new Physics()));
         lastRenderTickTime = System.currentTimeMillis();
     }
     private boolean running = false;
@@ -71,6 +75,7 @@ public class DerpyGame implements Runnable, MouseListener {
         long now = System.currentTimeMillis();
         if (!stopPhysics) {
             doPhysics(now - lastRenderTickTime);
+            //doPhysics(1);
         }
         lastRenderTickTime = now;
         doRender();
@@ -93,10 +98,64 @@ public class DerpyGame implements Runnable, MouseListener {
                 double dx = box.physics.vx * delta;
                 double dy = box.physics.vy * delta;
 
-                box.bounds.setRect(box.bounds.getX() + dx, box.bounds.getY() + dy, box.bounds.getWidth(), box.bounds.getHeight());
+                //flags to update the velocity later.
+                boolean collidesX = false;
+                boolean collidesY = false;
 
+                for (Box box1 : boxes) {
+                    if (box1.physics == null || box1 == box) {
+                        continue;
+                    }
+                    double px, py;
+                    //first, verify X
+                    //get the moved collision box
+                    Rectangle2D shiftX = (Rectangle2D) box.bounds.clone(); //clone so we don't modify the original
+                    shiftRectangle(shiftX, dx, 0);
+                    //check if collides
+                    if (box1.bounds.intersects(shiftX)) {
+                        //collides, let's stop it
+                        collidesX = true;
+                        if (box.physics.vx > 0) {
+                            //going right
+                            //set px as the amount to shift so it doesn't collide
+                            px = box1.bounds.getMinX() - shiftX.getMaxX();
+                            box.updateBounds(dx + px, 0);
+                        } else {
+                            //going left
+                            //set px as the amount to shift so it doesn't collide
+                            px = shiftX.getMinX() - box1.bounds.getMaxX();
+                            box.updateBounds(dx - px, 0);
+                        }
+                    }
+
+                    //now for the Y
+                    Rectangle2D shiftY = (Rectangle2D) box.bounds.clone(); //clone so we don't modify the original
+                    shiftRectangle(shiftY, 0, dy);
+                    if (box1.bounds.intersects(shiftY)) {
+                        collidesY = true;
+                        if (box.physics.vy > 0) {
+                            py = box1.bounds.getMinY() - shiftY.getMaxY();
+                            box.updateBounds(0, dy + py);
+                        } else if (box.physics.vy < 0) {
+                            py = box1.bounds.getMaxY() - shiftY.getMinY();
+                            box.updateBounds(0, dy + py);
+                        }
+                    }
+                }
+                if (collidesX) {
+                    box.physics.vx = 0;
+                } else {
+                    box.updateBounds(dx, 0);
+                }
+
+                if (collidesY) {
+                    box.physics.vy = 0;
+                } else {
+                    box.updateBounds(0, dy);
+                }
                 cycles -= Physics.PRECISON;
             }
+
         }
     }
 
@@ -119,10 +178,16 @@ public class DerpyGame implements Runnable, MouseListener {
 
     @Override
     public void mouseClicked(MouseEvent e) {
-        stopPhysics = false;
+        if (stopPhysics) {
+            stopPhysics = false;
+        }else{
+            boxes.clear();
+            stopPhysics = true;
+            init();
+        }
     }
 
-    //<editor-fold defaultstate="collapsed" desc="not used MouseListener stuff">
+    //<editor-fold defaultstate="collapsed" desc="useless MouseListener stuff">
     @Override
     public void mousePressed(MouseEvent e) {
     }
@@ -139,6 +204,10 @@ public class DerpyGame implements Runnable, MouseListener {
     public void mouseExited(MouseEvent e) {
     }
     //</editor-fold>
+
+    private void shiftRectangle(Rectangle2D shiftX, double dx, double dy) {
+        shiftX.setRect(shiftX.getX() + dx, shiftX.getY() + dy, shiftX.getWidth(), shiftX.getHeight());
+    }
 
     private static class Box {
 
@@ -175,6 +244,10 @@ public class DerpyGame implements Runnable, MouseListener {
         private void render(Graphics2D g) {
             g.setColor(color);
             g.fill(bounds);
+        }
+
+        public void updateBounds(double dx, double dy) {
+            bounds.setRect(bounds.getX() + dx, bounds.getY() + dy, bounds.getWidth(), bounds.getHeight());
         }
     }
 
